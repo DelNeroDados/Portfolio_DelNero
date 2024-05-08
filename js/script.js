e80dff7119e38ccf4414bb9986a1d7328adec63a
LuCaDelNero <161067539+LuCaDelNero@users.noreply.github.com>
2024-05-08 19:36:26 -0300

    Versão_01_do_Site
    Add no dia 08/05/2024

---
+++ js/script.js
@@ -0,0 +1,113 @@
+/* ========== toggle icon navbar ==========*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/* ========== scroll sections active link ==========*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    });
+
+    /* ========== sticky navbar ==========*/
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* ========== remove toggle icon and navbar when click navbar link (scroll) ==========*/
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/* ========== scroll reveal ==========*/
+
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/* ========== typed js  ==========
+
+const typed = new Typed('.existing-class', {
+    strings: ["Análise de Dados", "Análise de BI", "Big data Developer", "Modelagem de Dados", "Web Scraping"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 500
+});
+
+var typed2 = new Typed('.another-existing-class', {
+    strings: ['First sentence.', 'Second sentence.'],
+    typeSpeed: 30
+});
+
+*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Analise de Dados', 'Analise de BI', 'Big data Developer', 'Modelagem de Dados', 'Web Scraping'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+    loopCount: Infinity,
+})
+
+
+var typed2 = new Typed('.element', {
+    strings: ['EXCELÊNCIA É UM ATO...', 'EXCELÊNCIA NÃO É UM ATO, MAS UM HÁBITO! '],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    
+  });
+  
+
+
+/* ========== Welcome  ==========*/
+
+$(document).ready(function(){
+    var count = 0;
+    var counter = setInterval(function(){
+        if(count <101){
+            $('.count').text(count + '%');
+            $('.loader').css('width', count + '%');
+            count++
+        } else {
+            clearInterval(counter)
+        }
+    },10)
+})
+
+
+
+
+
+
